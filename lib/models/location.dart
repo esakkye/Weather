ef2b2bf3117d9dced8cd14de294f92cfec9cd2dd
esakkye <karthinainar96@gmail.com>
2021-12-19 22:06:04 +0530

    model updates

---
+++ lib/models/location.dart
@@ -0,0 +1,8 @@
+class Location {
+  final String locationName;
+  final String locationTime;
+
+  Location(this.locationName, this.locationTime);
+}
+
+List<Location> locations = [Location('Tirunelveli', '2PM')];
